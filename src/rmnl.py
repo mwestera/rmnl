853ad0072d52ae71c13937aedac8eebc5ea7d36a
mwestera <m.westera@hum.leidenuniv.nl>
2024-07-11 10:54:50 +0200

    hello newlines

---
+++ src/rmnl.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import pyperclip
+import re
+
+import argparse
+import logging
+import sys
+import random
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    argparser = argparse.ArgumentParser(description='Remove newlines while optionally keeping double newlines and/or lines ending with punctuation.')
+    argparser.add_argument('text', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Input file; or default stdin. Is overruled by option -c.')
+    argparser.add_argument('-p', '--punct', action='store_true', help='To keep newlines after sentence-final punctuation mark.')
+    argparser.add_argument('-d', '--double', action='store_true', help='To keep double newlines, replacing them by single newlines.')
+    argparser.add_argument('-c', '--clipboard', action='store_true', help='To modify clipboard contents; overrides text parameter.')
+
+    args = argparser.parse_args()
+
+    if args.clipboard:
+        text = pyperclip.paste()
+    else:
+        text = args.text.read()
+
+    newline_marker = '!@#$*(&^%$#@!#$*($@#%*&%^#$@!$*'
+    while newline_marker in text:
+        logging.warning('Lol impossible.')
+        chars = list(newline_marker)
+        random.shuffle(chars)
+        newline_marker = ''.join(chars)
+
+    if args.punct:
+        text = re.sub(r'([.?!])\n', f'\1{newline_marker}', text)
+    if args.double:
+        text = re.sub(r'\n\n', newline_marker, text)
+
+    text = re.sub(r'\n', r' ', text)
+    text = re.sub(r' +', ' ', text)
+    text = re.sub(r'\n ', '\n', text)
+
+    if args.punct or args.double:
+        text = text.replace(newline_marker, '\n')
+
+    if args.clipboard:
+        pyperclip.copy(text)
+        logging.info("Clipboard contents replaced successfully.")
+    else:
+        print(text)
+
+
+if __name__ == '__main__':
+    main()
